f40eaf4c9ecddbe933c4c6426e1df867dac4656b
Ivan Baena <baenadev.io@gmail.com>
2020-06-11 00:07:39 -0400

    nls module completed

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const chalk = require('chalk');
+const path = require('path');
 
-fs.readdir(process.cwd(), (err, filenames) => {
+const targetDir = process.argv[2] || process.cwd();
+const { lstat } = fs.promises;
+
+fs.readdir(targetDir, async (err, filenames) => {
   if (err) {
     console.log(err);
   }
 
-  console.log(filenames);
+  const statPromises = filenames.map((filename) => {
+    return lstat(path.join(targetDir, filename));
+  });
+
+  const allStats = await Promise.all(statPromises);
+
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
+    if (stats.isFile()) {
+      console.log(chalk.blue(filenames[index]));
+    } else {
+      console.log(chalk.magenta(filenames[index]));
+    }
+  }
 });
